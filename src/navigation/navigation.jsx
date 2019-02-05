315426f6a4aeb64571a53ac22e6f7ffd59fa9f21
dbmarch <dbmarch@gmail.com>
2019-02-04 23:48:25 -0600

    use theme styles in nav bar.

--- src/navigation/navigation.jsx
+++ src/navigation/navigation.jsx
@@ -15,44 +15,57 @@ const styles = (theme) => ({
     flexGrow: 1
   },
   menuItem: {
-    paddingRight: theme.spacing.unit * 8,
-    textDecoration: 'none'
+    paddingRight: theme.spacing.unit * 8
   }
 })
+
+const HomeLink = (props) => <RouterLink to="/" {...props} />
+const SearchLink = (props) => <RouterLink to="/search" {...props} />
+const TextLink = (props) => <RouterLink to="/text" {...props} />
+const ButtonsLink = (props) => <RouterLink to="/buttons" {...props} />
+const TabLink = (props) => <RouterLink to="/tab" {...props} />
+const SnackbarLink = (props) => <RouterLink to="/snack-bar" {...props} />
 
 const Navigation = ({ classes }) => {
   return (
     <AppBar position="static" color="inherit" className={classes.navBar}>
       <Toolbar>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/">
-            Home
-          </RouterLink>
+        <Link className={classes.menuItem} component={HomeLink} variant="h6" color="inherit" noWrap underline="hover">
+          Home
         </Link>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/search">
-            search
-          </RouterLink>
+
+        <Link className={classes.menuItem} component={SearchLink} variant="h6" color="inherit" noWrap underline="hover">
+          Search
         </Link>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/buttons">
-            Buttons
-          </RouterLink>
+
+        <Link
+          className={classes.menuItem}
+          component={ButtonsLink}
+          variant="h6"
+          color="inherit"
+          noWrap
+          underline="hover"
+        >
+          Buttons
         </Link>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/text">
-            Text
-          </RouterLink>
+
+        <Link className={classes.menuItem} component={TextLink} variant="h6" color="inherit" noWrap underline="hover">
+          Text
         </Link>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/tab">
-            Tabs
-          </RouterLink>
+
+        <Link className={classes.menuItem} component={TabLink} variant="h6" color="inherit" noWrap underline="hover">
+          Tab
         </Link>
-        <Link className={classes.menuItem} variant="h6" color="inherit" noWrap>
-          <RouterLink className="nav-item-link" to="/snack-bar">
-            SnackBar
-          </RouterLink>
+
+        <Link
+          className={classes.menuItem}
+          component={SnackbarLink}
+          variant="h6"
+          color="inherit"
+          noWrap
+          underline="hover"
+        >
+          SnackBar
         </Link>
       </Toolbar>
     </AppBar>
