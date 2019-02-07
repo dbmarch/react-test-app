c053cead040805a468303d2e84c4106c14a5a1ba
dbmarch <dbmarch@gmail.com>
2019-02-07 06:24:18 -0600

    add another snackbar demo

--- src/containers/snack-bar-view.jsx
+++ src/containers/snack-bar-view.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import SnackBar from '../components/snack-bar-custom'
+import SnackBarDirection from '../components/snack-bar-direction'
+
 const TabView = (props) => (
-  <div>
-    <h2>Snackbar Demo</h2>
-    <SnackBar />
+  <div className="side-side">
+    <div>
+      <h2>Snackbar Demo</h2>
+      <SnackBar />
+    </div>
+
+    <div>
+      <h2>Snack Bar from different directions: </h2>
+      <SnackBarDirection />
+    </div>
   </div>
 )
 
